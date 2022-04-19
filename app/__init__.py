5b80d152009bbd8bb907645de620833aefa3771f
swapnilaryan97 <saiamthere@gmail.com>
2022-04-19 19:51:46 -0400

    bug fix

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,8 @@ from rfc3339 import rfc3339
 from flask import render_template, Flask, has_request_context, request
 from flask_bootstrap import Bootstrap5
 from flask_wtf.csrf import CSRFProtect
+from app.logging_config import log_con
+from app.cli import create_database, create_log_folder
 
 from app.auth import auth
 from app.auth import auth
@@ -51,6 +53,7 @@ def create_app():
     bootstrap = Bootstrap5(app)
     app.register_blueprint(simple_pages)
     app.register_blueprint(auth)
+    app.register_blueprint(log_con)
     app.context_processor(utility_text_processors)
     app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'Simplex'
     app.register_error_handler(404, page_not_found)
@@ -61,130 +64,9 @@ def create_app():
     db.init_app(app)
     # add command function to cli commands
     app.cli.add_command(create_database)
-
-    # Deactivate the default flask logger so that log messages don't get duplicated
-    app.logger.removeHandler(default_handler)
-
-    # get root directory of project
-    root = os.path.dirname(os.path.abspath(__file__))
-    # set the name of the apps log folder to logs
-    logdir = os.path.join(root, 'logs')
-    # make a directory if it doesn't exist
-    if not os.path.exists(logdir):
-        os.mkdir(logdir)
-    # set name of the log file
-    log_file = os.path.join(logdir, 'info.log')
-    debug_file = os.path.join(logdir, 'debug.log')
-
-    handler = logging.FileHandler(log_file)
-    handler_debug = logging.FileHandler(debug_file)
-    # Create a log file formatter object to create the entry in the log
-    formatter = RequestFormatter(
-        '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
-        '%(levelname)s in %(module)s: %(message)s'
-    )
-    # set the formatter for the log entry
-    handler.setFormatter(formatter)
-    handler_debug.setFormatter(formatter)
-    # Set the logging level of the file handler object so that it logs INFO and up
-    handler.setLevel(logging.INFO)
-    handler_debug.setLevel(logging.DEBUG)
-    # Add the handler for the log entry
-    app.logger.addHandler(handler)
-    app.logger.addHandler(handler_debug)
-
-
-    @app.before_request
-    def start_timer():
-        g.start = time.time()
-
-    @app.after_request
-    def log_request(response):
-        if request.path == '/favicon.ico':
-            return response
-        elif request.path.startswith('/static'):
-            return response
-        elif request.path.startswith('/bootstrap'):
-            return response
-
-        now = time.time()
-        duration = round(now - g.start, 2)
-        dt = datetime.datetime.fromtimestamp(now)
-        timestamp = rfc3339(dt, utc=True)
-
-        ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-        host = request.host.split(':', 1)[0]
-        args = dict(request.args)
-
-        log_params = [
-            ('method', request.method),
-            ('path', request.path),
-            ('status', response.status_code),
-            ('duration', duration),
-            ('time', timestamp),
-            ('ip', ip),
-            ('host', host),
-            ('params', args)
-        ]
-
-        request_id = request.headers.get('X-Request-ID')
-        if request_id:
-            log_params.append(('request_id', request_id))
-
-        parts = []
-        for name, value in log_params:
-            part = name + ': ' + str(value) + ', '
-            parts.append(part)
-        line = " ".join(parts)
-        #this triggers a log entry to be created with whatever is in the line variable
-        app.logger.info(line)
-        error(app, response)
-
-        return response
-
+    app.cli.add_command(create_log_folder)
     return app
 
-
-def debug():
-    pass
-
-def error(app, response):
-    # pass
-    now = time.time()
-    duration = round(now - g.start, 2)
-    dt = datetime.datetime.fromtimestamp(now)
-    timestamp = rfc3339(dt, utc=True)
-    health = 'Healthy' if response.status_code==200 else 'NOT HEALTHY!'
-
-    ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-    host = request.host.split(':', 1)[0]
-    args = dict(request.args)
-
-    log_params = [
-        ('method', request.method),
-        ('path', request.path),
-        ('health', health),
-        ('status', response.status_code),
-        ('duration', duration),
-        ('time', timestamp),
-        ('ip', ip),
-        ('host', host),
-        ('params', args)
-    ]
-
-    request_id = request.headers.get('X-Request-ID')
-    if request_id:
-        log_params.append(('request_id', request_id))
-
-    parts = []
-    for name, value in log_params:
-        part = name + ': ' + str(value) + ', '
-        parts.append(part)
-    line = " ".join(parts)
-    # this triggers a log entry to be created with whatever is in the line variable
-    app.logger.debug(line)
-
-    return
 
 
 @login_manager.user_loader
